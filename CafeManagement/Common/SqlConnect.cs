6d9b96ae5bf386b789322b32862539ccaa0d8ec2
giangoc <40663003+giangoc@users.noreply.github.com>
2020-02-24 14:24:35 +0700

    add new class

---
+++ CafeManagement/Common/SqlConnect.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CafeManagement.Common
+{
+    public class SqlConnect
+    {
+        SqlConnection connect = new SqlConnection(ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString);
+        public void ConnectionOpen()
+        {
+            connect.Open();
+        }
+
+        public void ConnectionClose()
+        {
+            connect.Close();
+        }
+    }
+}
---
+++ CafeManagement/Dao/MenuManagementDao.cs
@@ -0,0 +1,12 @@
+﻿using CafeManagement.Models;
+
+namespace CafeManagement.Dao
+{
+    public class MenuManagementDao
+    {
+        public int Add(MenuModel model)
+        {
+            return SuccessCode;
+        }
+    }
+}
