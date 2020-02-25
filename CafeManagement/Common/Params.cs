e776e33b34f3630cd4527f978b2f28e420d1d1ed
giangoc <40663003+giangoc@users.noreply.github.com>
2020-02-25 11:52:03 +0700

    change

---
+++ CafeManagement/Common/Params.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+namespace CafeManagement.Common
+{
+    public class Params
+    {
+        public string connect = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+
+        public int successCode = 0;
+        public int failCode = 1;
+        public int expCode = -1;
+    }
+}
